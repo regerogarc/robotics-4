ef61028bbd3c5bc4f5a362c3389cfcb2dd4c0a33
Finlay Durkin <finlaydurkin@gmail.com>
2024-03-07 16:14:00 +0000

    MIDI parser

---
+++ Sound/Music.c
@@ -0,0 +1,9 @@
+task play_sound()
+{
+	int timing_multiplier = 1;
+	playTone(739.99,0 * timing_multiplier);
+	playTone(554.37,384 * timing_multiplier);
+	playTone(493.88,383 * timing_multiplier);
+	playTone(440.0,383 * timing_multiplier);
+	playTone(369.99,191 * timing_multiplier);
+}
---
+++ freebird.c
@@ -0,0 +1,34 @@
+#define C4		261.63
+#define C4_		277.18
+#define D4		293.66
+#define D4_		311.13
+#define E4		329.63
+#define F4		349.23
+#define	F4_		369.99
+#define G4		392
+#define G4_		415.3
+#define A4		440
+#define A4_		466.16
+#define B4		493.88
+#define C5		523.25
+#define C5_		554.37
+#define D5		587.33
+#define D5_		622.25
+#define E5		659.25
+#define F5		698.46
+#define F5_		739.99
+#define G5		783.99
+#define G5_		830.61
+#define	A5		880
+#define A5_		932.33
+#define	B5		987.77
+#define C6		1046.5
+
+
+
+task main()
+{
+
+
+
+}
